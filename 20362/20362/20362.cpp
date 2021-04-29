43868c8f6e8d1ec32070e8ca19e4c654e09be5df
kookeej <hijiny98@naver.com>
2021-04-30 00:41:29 +0900

    initial commit

---
+++ 20362/20362/20362.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class Chatting {
+	string id;
+	string chat;
+public:
+	Chatting() { 
+		id = "";
+		chat = "";
+	}
+	string getId() { return id; }
+	string getChat() { return chat; }
+	void setId(string id) { this->id = id; }
+	void setChat(string chat) { this->chat = chat; }
+};
+
+int main()
+{
+	int n;
+	string s;
+	string id, chat;
+	string answer;
+	cin >> n >> s;
+	
+	Chatting* ch = new Chatting[n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> id >> chat;
+		if (id == s) {
+			//num = i + 1;	//정답자의 순서
+			answer = chat;
+		}
+		ch[i].setId(id);
+		ch[i].setChat(chat);
+	}
+
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		if (ch[i].getId() == s) break;
+		if (ch[i].getChat() == answer) {
+			cnt += 1;
+		}
+	}
+
+	cout << cnt;
+}
